35ffcc31b676be7f79d386185f498cb0f1bf1039
blueloot <goatfishgames@gmail.com>
2021-03-16 17:25:07 +0100

    movement

--- script/Player.cs
+++ script/Player.cs
@@ -3,19 +3,81 @@ using System;
 
 public class Player : KinematicBody
 {
+    // Properties
+    [Export] private float Gravity = 42f;
+    [Export] private float MaxSlopeThresholdAllowed = 0.7f;
+    [Export] private float MaxStepThresholdAllowed = 10f;
+    public Vector3 GroundVector;
+    public Vector3 Velocity;
+    public Vector3 MoveDirection;
+    public bool Grounded;
+
+    private float GroundCheckDistance = 5f;
+    private float GroundSnap;
+
+    private CapsuleShape Body;
+
+    // Singletons
     PlayerInput PlayerInput;
-    Mouse Mouse;
+    Mouse Mouse; // temp
 
     public override void _Ready()
     {
         PlayerInput = GetNode<PlayerInput>("/root/PlayerInput");
-        Mouse = GetNode<Mouse>("/root/Mouse");
+        Mouse = GetNode<Mouse>("/root/Mouse"); // temp
 
-        Mouse.Hide();
+        Body = GetNode<CollisionShape>("Body").Shape as CapsuleShape;
+
+        GroundSnap = GroundCheckDistance;
+
+        Mouse.Hide(); // temp
     }
 
-    public override void _Process(float delta)
+    public override void _PhysicsProcess(float delta)
     {
 
+        // Ground check
+        if (IsOnFloor())
+        {
+            Grounded = true;
+            GroundVector = -GetFloorNormal() * GroundSnap;
+            GroundSnap = GroundCheckDistance;
+        }
+        else
+        {
+            Grounded = false;
+            GroundVector += Vector3.Down * Gravity * delta;
+        }
+
+        // Ground Control
+        if (PlayerInput.GetJump() && Grounded)      // jumping
+        {
+            Grounded = false;
+            GroundSnap = 1f;
+            GroundVector = Vector3.Up * 12f;
+        }
+        if (GetSlideCount() > 0 && Grounded)        // stairs
+        {
+            for (int i = 0; i < GetSlideCount(); i++)
+            {
+                var col = GlobalTransform.origin.y - GetSlideCollision(i).Position.y;
+                var stairjump = Gravity/10;
+                var bodysize = Body.Height/2;
+                var stairnormal = Body.Height/MaxStepThresholdAllowed;
+
+                if (col > bodysize - stairnormal && col < bodysize + stairnormal)
+                {
+                    if (MoveDirection != Vector3.Zero)
+                    {
+                        GroundSnap = 1f;
+                        GroundVector = Vector3.Up * stairjump;
+                    }
+                }
+            }
+        }
+
+        // update
+        var linvel = new Vector3(Velocity.x + GroundVector.x, GroundVector.y, Velocity.z + GroundVector.z);
+        MoveAndSlide(linvel, Vector3.Up, false, 4, MaxSlopeThresholdAllowed, false);
     }
 }
--- script/PlayerWalking.cs
+++ script/PlayerWalking.cs
@@ -3,19 +3,44 @@ using System;
 
 public class PlayerWalking : Node
 {
-    [Export] public float MovementSpeed = 3f;
+    // Nodes
+    [Export] private NodePath PlayerBody = "..";
+    private KinematicBody Body;
+    private Spatial Head;
 
+    // Exports
+    [Export] public float MovementSpeed = 7f;
+    [Export] private float Accelleration = 7f;
+    [Export] private float Friction = 0.9f;
+
+    // Singletons
     PlayerInput PlayerInput;
-    Mouse Mouse;
+    
 
     public override void _Ready()
     {
         PlayerInput = GetNode<PlayerInput>("/root/PlayerInput");
-        Mouse = GetNode<Mouse>("/root/Mouse");
+
+        Body = GetNode<KinematicBody>(PlayerBody);
+        Head = GetNode<Spatial>(PlayerBody+"/Head");
     }
 
-    public override void _Process(float delta)
+    public override void _PhysicsProcess(float delta)
     {
+        Player Player = GetNode<Player>(PlayerBody);
 
+        var movement = PlayerInput.GetMovement();
+        var transform = Player.GlobalTransform;
+
+        Player.MoveDirection = (Player.Grounded) ? Vector3.Zero : Player.MoveDirection;
+        Player.MoveDirection += -transform.basis.z * movement.y;
+        Player.MoveDirection +=  transform.basis.x * movement.x;
+        Player.MoveDirection = Player.MoveDirection.Normalized();
+
+        var targetVel = Player.MoveDirection * MovementSpeed;
+
+        Player.Velocity = Player.Velocity.LinearInterpolate( targetVel, Accelleration * delta);
+        
+        if (movement == Vector2.Zero) Player.Velocity *= Friction;
     }
 }
